f005bf3a215b5f8156cb9f433fd7ab8d203d3906
imsekhar4545 <135575542+imsekhar4545@users.noreply.github.com>
2023-10-06 10:33:34 +0530

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -5,10 +5,6 @@ const {
 } = require("../controllers/analyticsController");
 
 const router = express.Router();
-
-//routes
-
-//GET BLOOD DATA
 router.get("/bloodGroups-data", authMiddelware, bloodGroupDetailsContoller);
 
 module.exports = router;
