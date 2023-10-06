e43450cda736554d34259faf85af1ec38c28a5a2
imsekhar4545 <135575542+imsekhar4545@users.noreply.github.com>
2023-10-06 10:35:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,15 +7,8 @@ const {
 const authMiddelware = require("../middlewares/authMiddelware");
 
 const router = express.Router();
-
-//routes
-//REGISTER || POST
 router.post("/register", registerController);
-
-//LOGIN || POST
 router.post("/login", loginController);
-
-//GET CURRENT USER || GET
 router.get("/current-user", authMiddelware, currentUserController);
 
 module.exports = router;
